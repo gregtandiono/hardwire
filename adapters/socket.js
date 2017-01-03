d2a6ebc18f39a9fac21c128d879e61fee2444993
gregtandiono <gregtandiono@gmail.com>
2017-01-03 11:54:17 +0700

    abstracted general socket connection

---
+++ adapters/socket.js
@@ -0,0 +1,28 @@
+/**
+ * adapter.js
+ *
+ * [SOCKET ADAPTER]
+ *
+ * This is an instance of the socket server,
+ * running on a different port
+ */
+
+var env = "development";
+if (process.env.NODE_ENV) {
+  env = process.env.NODE_ENV;
+  console.log("socket acknowledges node environment:", env);
+}
+
+var config = require("../config/" + env + ".json");
+var io = require("socket.io")(config.socket);
+var socketAdapter = null;
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.emit("initiation", {message: "connected to socket server"});
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+    socket.emit("user disconnected");
+  })
+});
+
+module.exports = io;
