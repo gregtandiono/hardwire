2894b06af05381b47beef9ee6e2ed4c5d5f99491
gregtandiono <gregtandiono@gmail.com>
2017-01-04 14:05:26 +0700

    added main react app wrapper for operator client

---
+++ clients/operator/javascripts/src/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Operator App</h1>
+      </div>
+    )
+  }
+}
+
+const mountNode = document.getElementById("main-react-app")
+
+ReactDOM.render(<App />, mountNode)
