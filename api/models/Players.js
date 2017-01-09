4e584bbda47ad297d2edc63b10e7aadae8021c80
gregtandiono <gregtandiono@gmail.com>
2017-01-09 16:09:38 +0700

    created Players model

---
+++ api/models/Players.js
@@ -0,0 +1,30 @@
+/**
+ * Players.js
+ *
+ * [MODEL]
+ */
+
+var Promise       = require("bluebird")
+  , _             = require("underscore")
+  , pg            = require("../adapters/db")
+  , BaseModel     = require("./BaseModel");
+
+var attributes = {
+  id          : {value: "", type: "uuid", required: true},
+  name        : {value: "", type: "string", required: true},
+  cellphone   : {value: "", type: "string"},
+  ym          : {value: "", type: "string"},
+  email       : {value: "", type: "email"},
+  notes       : {value: "", type: "string"},
+  operator_id : {value: "", type: "UUID", required: true},
+};
+
+class Players extends BaseModel {
+  constructor() {
+    super();
+    this.attributes = attributes;
+    this.table = "players";
+  }
+}
+
+module.exports = Players;
