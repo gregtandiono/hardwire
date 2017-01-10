dd9fe02172f65705a65134e4ed2115e66befa777
gregtandiono <gregtandiono@gmail.com>
2017-01-10 19:00:01 +0700

    fixed endpoint auth middleware placement

--- server.js
+++ server.js
@@ -23,7 +23,11 @@ var config = require("./config/" + env + ".json");
 app.config = config;
 const baseApiURL = "/api";
 
+// LOAD MIDDLEWARES
+var auth = require("./api/middlewares/auth");
+
 // LOAD ENDPOINTS
+var login = require("./api/endpoints/login");
 var users = require("./api/endpoints/users");
 var players = require("./api/endpoints/players");
 var games = require("./api/endpoints/games");
@@ -31,8 +35,6 @@ var sites = require("./api/endpoints/sites");
 var transactions = require("./api/endpoints/transactions");
 var banks = require("./api/endpoints/banks");
 
-// LOAD MIDDLEWARES
-var auth = require("./api/middlewares/auth");
 
 // APPLY MIDDLEWARES
 app.use(compression());
@@ -40,17 +42,17 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors);
+app.use(cors());
 app.use(express.static("public"));
 
 // APPLY ENDPOINTS
-app.use(baseApiURL + "/auth", users);
-app.use(baseApiURL + "/users", users, auth);
-app.use(baseApiURL + "/players", players, auth);
-app.use(baseApiURL + "/games", games, auth);
-app.use(baseApiURL + "/sites", sites, auth);
-app.use(baseApiURL + "/transactions", transactions, auth);
-app.use(baseApiURL + "/banks", banks, auth);
+app.use(baseApiURL + "/login", login);
+app.use(baseApiURL + "/users", auth, users);
+app.use(baseApiURL + "/players", auth, players);
+app.use(baseApiURL + "/games", auth, games);
+app.use(baseApiURL + "/sites", auth, sites);
+app.use(baseApiURL + "/transactions", auth, transactions);
+app.use(baseApiURL + "/banks", auth, banks);
 
 // SETUP STATIC FILE SERVING
 app.get("*", (req, res) => {
