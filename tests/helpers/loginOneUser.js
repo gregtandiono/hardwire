4234fd54bb1385591a81a7ead9fcae173b98a1a0
gregtandiono <gregtandiono@gmail.com>
2017-01-10 10:46:07 +0700

    added loginOneUser test helper method that will return a valid token

--- tests/helpers/loginOneUser.js
+++ tests/helpers/loginOneUser.js
@@ -1,3 +1,38 @@
 /**
  * loginOneUser.js
+ *
+ * [TEST | HELPER]
  */
+
+var chai           = require("chai")
+ , assert         = chai.assert
+ , expect         = chai.expect
+ , should         = chai.should()
+ , chaiAsPromised = require("chai-as-promised")
+ , chaiHttp       = require("chai-http");
+
+chai.use(chaiAsPromised);
+chai.use(chaiHttp);
+
+var app = require("../../server");
+
+function loginOneUser() {
+  return new Promise((resolve, reject) => {
+    chai.request(app)
+      .post("/api/users/auth")
+      .send({
+        username: "snafu01",
+        password: "theLongestPasswordOnFuckingEarth"
+      })
+      .end((err, res) => {
+        // at this point the response from the server should be a user_id and a token
+        if (err) {
+          console.log("failed to login user from helper");
+          reject(err);
+        }
+        resolve(res.body);
+      })
+  })
+}
+
+module.exports = loginOneUser
