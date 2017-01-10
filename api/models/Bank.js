6fe33908136f7668522aad47b3df5fb19693e404
gregtandiono <gregtandiono@gmail.com>
2017-01-11 00:47:45 +0700

    Bank model is updated with new column

--- api/models/Bank.js
+++ api/models/Bank.js
@@ -10,15 +10,16 @@ var Promise       = require("bluebird")
   , BaseModel     = require("./BaseModel");
 
 var attributes = {
-  id             : {value: "", type: "uuid", required: true},
-  name           : {value: "", type: "string", required: true},
-  player_id      : {value: "", type: "uuid", required: true},
-  operator_id    : {value: "", type: "uuid", required: true},
-  other_name     : {value: "", type: "string"},
-  account_holder : {value: "", type: "string", required: true},
-  account_number : {value: "", type: "string", required: true},
-  username       : {value: "", type: "string", required: true},
-  password       : {value: "", type: "string", required: true},
+  id               : {value: "", type: "uuid", required: true},
+  name             : {value: "", type: "string", required: true},
+  player_id        : {value: "", type: "uuid", required: true},
+  operator_id      : {value: "", type: "uuid", required: true},
+  other_name       : {value: "", type: "string"},
+  account_holder   : {value: "", type: "string", required: true},
+  account_number   : {value: "", type: "string", required: true},
+  system_ownership : {value: "", type: "string", required: true},
+  username         : {value: "", type: "string", required: true},
+  password         : {value: "", type: "string", required: true},
 };
 
 class Bank extends BaseModel {
