c3de10c10441215d9f2b6f6fefb4771301322f84
gregtandiono <gregtandiono@gmail.com>
2017-02-20 22:15:13 +0700

    created user REDUX actions

---
+++ public/javascripts/src/constants/users.js
@@ -0,0 +1,13 @@
+/**
+ * UserConstants.js
+ *
+ * [CONSTANT]
+ */
+
+export const USER_LOGIN = "USER_LOGIN";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
+
+export const USER_GET_INFO = "USER_GET_INFO";
+export const USER_GET_INFO_SUCCESS = "USER_GET_INFO_SUCCESS";
+export const USER_GET_INFO_FAIL = "USER_GET_INFO_FAIL";
