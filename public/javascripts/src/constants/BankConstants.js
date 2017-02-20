e67a512165dbed0f8c04c018fc884b02ad0f4c2e
gregtandiono <gregtandiono@gmail.com>
2017-02-21 00:42:06 +0700

    created Banks REDUX constants

---
+++ public/javascripts/src/constants/BankConstants.js
@@ -0,0 +1,35 @@
+/**
+ * BankConstants.js
+ *
+ * [CONSTANT]
+ */
+
+export const USER_LOGIN = "USER_LOGIN";
+export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
+export const USER_LOGIN_FAIL = "USER_LOGIN_FAIL";
+
+// Fetch all banks related to player
+
+export const FETCH_ALL_BANKS = "FETCH_ALL_BANKS"
+export const FETCH_ALL_BANKS_SUCCESS = "FETCH_ALL_BANKS_SUCCESS"
+export const FETCH_ALL_BANKS_FAIL = "FETCH_ALL_BANKS_FAIL"
+
+// Fetch one bank related to player
+
+export const FETCH_ONE_BANK = "FETCH_ONE_BANK"
+export const FETCH_ONE_BANK_SUCCESS = "FETCH_ONE_BANK_SUCCESS"
+export const FETCH_ONE_BANK_FAIL = "FETCH_ONE_BANK_FAIL"
+
+export const CREATE_BANK = "CREATE_BANK"
+export const CREATE_BANK_SUCCESS = "CREATE_BANK_SUCCESS"
+export const CREATE_BANK_FAIL = "CREATE_BANK_FAIL"
+
+export const UPDATE_BANK = "UPDATE_BANK"
+export const UPDATE_BANK_SUCCESS = "UPDATE_BANK_SUCCESS"
+export const UPDATE_BANK_FAIL = "UPDATE_BANK_FAIL"
+
+export const DELETE_BANK = "DELETE_BANK"
+export const DELETE_BANK_SUCCESS = "DELETE_BANK_SUCCESS"
+export const DELETE_BANK_FAIL = "DELETE_BANK_FAIL"
+
+
