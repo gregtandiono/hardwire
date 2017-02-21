3ef0fc03eff75fa828ba379ac890edef8baf2caf
gregtandiono <gregtandiono@gmail.com>
2017-02-21 20:34:50 +0700

    added facebook's react table

---
+++ public/javascripts/src/components/PlayerListView.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+export default class PlayerListView extends Component {
+    render() {
+        return (
+            <div />
+        )
+    }
+}
+
---
+++ public/javascripts/src/components/UITableView.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+export default class UITableView extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        )
+    }
+}
