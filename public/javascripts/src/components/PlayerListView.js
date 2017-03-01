fd3d1bccc47c4f89be3e2f218bac5ad836ce5141
gregtandiono <gregtandiono@gmail.com>
2017-03-01 13:19:26 +0700

    frank zappa says fuck you redux

--- public/javascripts/src/components/PlayerListView.js
+++ public/javascripts/src/components/PlayerListView.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react"
 import ReactDOM from "react-dom"
+import * as PlayerActions from "../actions/PlayerActions"
 import UITableView from "./UITableView"
 
-var fakeData = [
-    { name: "gregory tandiono", age: 27, username: "gregtandiono" },
-    { name: "evelyn thong", age: 34, username: "evethong" },
-    { name: "mini lab", age: 26, username: "miniLovesBini" },
-    { name: "aloenk", age: 40, username: "whatevsBruv" }
-]
+// var fakeData = [
+//     { name: "gregory tandiono", age: 27, username: "gregtandiono" },
+//     { name: "evelyn thong", age: 34, username: "evethong" },
+//     { name: "mini lab", age: 26, username: "miniLovesBini" },
+//     { name: "aloenk", age: 40, username: "whatevsBruv" }
+// ]
 
 export default class PlayerListView extends Component {
+    componentDidMount() {
+        this.props.dispatch(PlayerActions.fetchAllPlayersAsync());
+    }
+
     render() {
         return (
             <div>
                 <h2>Player List</h2>
                 <UITableView
-                    headers={["name", "age", "username"]}
-                    body={fakeData} />
+                    headers={["name", "ym", "username"]}
+                    body={this.props.body} />
             </div>
         )
     }
---
+++ public/javascripts/src/containers/PlayerListContainer.js
@@ -0,0 +1,25 @@
+/**
+ * PlayerListContainer.js
+ *
+ * [CONTAINER]
+ *
+ */
+
+import React, {Component} from "react";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+import PLayerListView from "../components/PlayerListView";
+
+function mapStateToProps(state) {
+  var { fetchAllPlayers } = state;
+  var { players } = fetchAllPlayers;
+  return {
+    players 
+  }
+}
+
+PlayerListView.contextTypes = { // this context type is to grant access to the react router object for redirection
+  router: React.PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps)(PlayerListView)
