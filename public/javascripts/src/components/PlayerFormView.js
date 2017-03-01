01667d6638ed1ca2c4535b59b0289e51d02dfd56
gregtandiono <gregtandiono@gmail.com>
2017-03-01 14:56:33 +0700

    wtf

--- public/javascripts/src/components/PlayerFormView.js
+++ public/javascripts/src/components/PlayerFormView.js
@@ -17,6 +17,15 @@ const inputs = [
     {name: "notes", label: "Notes"},
 ]
 
+const bankInputs = [
+    {name: "bank_name", label: "Bank Name"},
+    {name: "bank_other_name", label: "Other Bank Name (if none is applicable)"},
+    {name: "bank_account_holder", label: "Account Holder"},
+    {name: "bank_account_number", label: "Account Number"},
+    {name: "bank_username", label: "Username"},
+    {name: "bank_password", label: "Password"},
+]
+
 export default class PlayerFormView extends UIFormController {
     componentWillReceiveProps(nextProps) {
         if (!nextProps.error && nextProps.postSuccess && !nextProps.loading) {
@@ -31,6 +40,8 @@ export default class PlayerFormView extends UIFormController {
                 <h2>Create new Player</h2>
                 <form onSubmit={this._submitHandler.bind(this, PlayerActions.createPlayerAsync)}>
                     {this._renderInputs(inputs)}
+                    <h3>Bank Details</h3>
+                    {this._renderInputs(bankInputs)}
                     {this._renderButton()}
                 </form>
             </div>
